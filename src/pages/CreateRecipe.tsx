f202bd1e3681d819173264419f346c72b4b1e372
Rizki Alfarid <gusnafarid@gmail.com>
2023-04-13 11:58:01 +0700

    add create recipe form

--- src/pages/CreateRecipe.tsx
+++ src/pages/CreateRecipe.tsx
@@ -1,7 +1,51 @@
 import React from "react";
 
 const CreateRecipe = () => {
-  return <div>Create Recipe</div>;
+  return (
+    <div className="card w-full max-w-md mt-16 mx-auto shadow bg-base-100">
+      <div className="card-body">
+        <h2 className="card-title">Create Recipe</h2>
+        <form className="form-control w-full gap-3">
+          <div className="form-control">
+            <label className="label" htmlFor="name">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter recipe name"
+              className="input input-bordered w-full"
+              name="name"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label" htmlFor="instruction">
+              <span className="label-text">Instruction</span>
+            </label>
+            <textarea
+              className="textarea textarea-bordered"
+              name="instruction"
+              rows={7}
+            ></textarea>
+          </div>
+          <div className="form-control">
+            <label className="label" htmlFor="cooking-time">
+              <span className="label-text">Cooking time (minute)</span>
+            </label>
+            <input
+              className="input input-bordered"
+              type="number"
+              name="cooking-time"
+              id="cookingTime"
+              min={1}
+            />
+          </div>
+          <button className="btn btn-primary btn-block my-4" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default CreateRecipe;
