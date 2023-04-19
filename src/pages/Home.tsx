fdf565bfb8697711f1705e23b9d6063617cf2277
Rizki Alfarid <gusnafarid@gmail.com>
2023-04-19 15:20:30 +0700

    add recipe list to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import RecipeCard from "../components/RecipeCard";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [recipeList, setRecipeList] = useState([]);
+
+  const getRecipes = async () => {
+    try {
+      const res = await axios.get("http://localhost:3001/recipes");
+      setRecipeList(res.data);
+    } catch (error) {
+      console.log(error);
+      alert("Error fetching recipes");
+    }
+  };
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  return (
+    <div className="w-full max-w-7xl mx-auto flex flex-col gap-4 items-center pt-8">
+      {recipeList.length > 0 &&
+        recipeList.map(({ imgUrl, name, cookingTime }) => (
+          <RecipeCard imgUrl={imgUrl} name={name} cookingTime={cookingTime} />
+        ))}
+    </div>
+  );
 };
 
 export default Home;
